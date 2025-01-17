3983757f56d03b67bf6a825c2cc3ab6d8b24f736
hwk9407 <hwk__@naver.com>
2025-01-17 18:03:10 +0900

    Feat(User): User 엔티티 및 UserRepository 추가
    - 유저의 역할을 enum으로 관리하기위해 Authority 클래스 추가

---
+++ src/main/java/com/example/javabackendonboarding/domain/user/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.javabackendonboarding.domain.user.entity;
+
+import com.example.javabackendonboarding.domain.user.enums.Authority;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Getter
+@Entity(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private Set<Authority> authorityName;
+
+    public User(String username, String password, String nickname, Set<Authority> authorityName) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.authorityName = authorityName;
+    }
+}
