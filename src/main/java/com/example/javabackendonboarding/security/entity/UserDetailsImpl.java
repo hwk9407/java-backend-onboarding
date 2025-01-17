8b565e4d633a8e070d246d53cefa9baab702bc65
hwk9407 <hwk__@naver.com>
2025-01-17 21:49:48 +0900

    Feat(UserRepository): findByUsername 추가 및 UserDetails, UserDetailsService 구현객체 추가

---
+++ src/main/java/com/example/javabackendonboarding/security/entity/UserDetailsImpl.java
@@ -0,0 +1,63 @@
+package com.example.javabackendonboarding.security.entity;
+
+import com.example.javabackendonboarding.domain.user.entity.User;
+import com.example.javabackendonboarding.domain.user.enums.Authority;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Getter
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Authority> roles = user.getAuthorityName();
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        roles.forEach(role -> {
+            String authority = role.toString();
+            authorities.add(new SimpleGrantedAuthority(authority));
+        });
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
