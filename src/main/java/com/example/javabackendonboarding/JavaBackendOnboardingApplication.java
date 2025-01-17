1bbbb4d73e716a29835138a4c05764e3e778779a
hwk9407 <hwk__@naver.com>
2025-01-17 16:17:33 +0900

    git init

---
+++ src/main/java/com/example/javabackendonboarding/JavaBackendOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabackendonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendOnboardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javabackendonboarding/JavaBackendOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabackendonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
