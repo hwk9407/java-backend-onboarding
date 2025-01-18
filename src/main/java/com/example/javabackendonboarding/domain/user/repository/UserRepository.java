f1936fc0655cf3ff9d3ad9736839be2784a76cf3
hwk9407 <hwk__@naver.com>
2025-01-18 21:17:02 +0900

    Feat(UserRepository): 로그인 인증 시 쿼리가 묶여 나갈 수 있도록 EntityGraph 어노테이션 추가

--- src/main/java/com/example/javabackendonboarding/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/javabackendonboarding/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.javabackendonboarding.domain.user.repository;
 
 import com.example.javabackendonboarding.domain.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
+    @EntityGraph(attributePaths = "authorityName")
     Optional<User> findByUsername(String username);
 }
